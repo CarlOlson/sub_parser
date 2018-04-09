26ea7971c5bb9b1005fa1a5e7093019253173117
CarlOlson <CarlOlson@users.noreply.github.com>
2018-04-09 13:18:14 -0500

    add parse to timestamp

--- lib/sub_parser/timestamp.rb
+++ lib/sub_parser/timestamp.rb
@@ -7,10 +7,10 @@ module SubParser
     def initialize hours = 0, minutes = 0, seconds = 0, milliseconds = 0
       @hours, @minutes, @seconds, @milliseconds = 0, 0, 0, 0
 
-      self.milliseconds = milliseconds
-      self.seconds += seconds
-      self.minutes += minutes
-      self.hours += hours
+      self.milliseconds = milliseconds.to_i
+      self.seconds += seconds.to_i
+      self.minutes += minutes.to_i
+      self.hours += hours.to_i
     end
 
     def to_s
@@ -34,6 +34,20 @@ module SubParser
 
     def hours= value
       @hours = value
+    end
+
+    def eql? other
+      [ hours == other.hours,
+        minutes == other.minutes,
+        seconds == other.seconds,
+        milliseconds == other.milliseconds
+      ].all?
+    end
+
+    def self.parse raw
+      if raw =~ /(\d+):(\d\d):(\d\d),(\d\d\d)/
+        Timestamp.new $1, $2, $3, $4
+      end
     end
 
   end
